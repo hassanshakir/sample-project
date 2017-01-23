ea169a44bcdaa8d3c59ff280b2e033a3c4b04e9e
hassanshakir <hassan.shakir@hotmail.com>
2017-01-23 17:24:29 +0500

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,10 @@
+<form name="ecom" method="post" action="https://test.timesofmoney.com/direcpay/secure/PaymentTransactionServlet"> 
+	<input type="hidden" name=" MerchantID " value="12345"> 
+	<input type="submit" name="submit" value="Submit"> 
+</form>
+
+<?php
+
+echo 3 << 5;
+
+?>
